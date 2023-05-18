2276bb4766416b78869117ce6828ff9d02d90c32
LuchoN83 <39603304+LuchoN83@users.noreply.github.com>
2023-05-18 15:19:42 -0500

    Update 12 -azurecosmos.tf

--- terraform-manifests/12 -azurecosmos.tf
+++ terraform-manifests/12 -azurecosmos.tf
@@ -1,7 +1,7 @@
 resource "azurerm_cosmosdb_account" "db" {
   name = "retocosmosdb"
-  location = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  location = azurerm_resource_group.aks_rg.location
+  resource_group_name = azurerm_resource_group.aks_rg.name
   offer_type = "Standard"
   kind = "GlobalDocumentDB"
 
@@ -10,20 +10,20 @@ resource "azurerm_cosmosdb_account" "db" {
   }
 
   geo_location {
-    location = azurerm_resource_group.rg.location
+    location = azurerm_resource_group.aks_rg.location
     failover_priority = 0
   }
 }
 
 resource "azurerm_cosmosdb_sql_database" "db" {
   name = var.cosmosdbname
-  resource_group_name = azurerm_resource_group.rg.name
+  resource_group_name = azurerm_resource_group.aks_rg.name
   account_name = azurerm_cosmosdb_account.db.name
 }
 
 resource "azurerm_cosmosdb_sql_container" "container" {
   name = var.cosmosdbcontainer
-  resource_group_name = azurerm_resource_group.rg.name
+  resource_group_name = azurerm_resource_group.aks_rg.name
   account_name = azurerm_cosmosdb_account.db.name
   database_name = azurerm_cosmosdb_sql_database.db.name
   partition_key_path = "/id"
