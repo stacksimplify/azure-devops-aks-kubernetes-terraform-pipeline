1805620719a93d5b5a4628dd42ed9acfd3eb2ebe
LuchoN83 <39603304+LuchoN83@users.noreply.github.com>
2023-05-18 15:12:41 -0500

    Update 12 -azurecosmos.tf

--- terraform-manifests/12 -azurecosmos.tf
+++ terraform-manifests/12 -azurecosmos.tf
@@ -1,1 +1,31 @@
+resource "azurerm_cosmosdb_account" "db" {
+  name = "velidatutorialcosmosdb"
+  location = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  offer_type = "Standard"
+  kind = "GlobalDocumentDB"
 
+  consistency_policy {
+      consistency_level = "Session"
+  }
+
+  geo_location {
+    location = azurerm_resource_group.rg.location
+    failover_priority = 0
+  }
+}
+
+resource "azurerm_cosmosdb_sql_database" "db" {
+  name = var.cosmosdbname
+  resource_group_name = azurerm_resource_group.rg.name
+  account_name = azurerm_cosmosdb_account.db.name
+}
+
+resource "azurerm_cosmosdb_sql_container" "container" {
+  name = var.cosmosdbcontainer
+  resource_group_name = azurerm_resource_group.rg.name
+  account_name = azurerm_cosmosdb_account.db.name
+  database_name = azurerm_cosmosdb_sql_database.db.name
+  partition_key_path = "/id"
+  throughput = 400
+}
