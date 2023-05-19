6d09f6a4ec47cbf9adbbcf61541117e1262ab822
Luis Navarrete <luis.navarrete.aranda@hotmail.com>
2023-05-19 01:13:41 -0500

    Creacion de equipo WIndows y cambio de clave MongoDB cambio de capacidad de disco

--- terraform-manifests/10-aks-cluster-windows-user-nodepools.tf
+++ terraform-manifests/10-aks-cluster-windows-user-nodepools.tf
@@ -9,7 +9,7 @@ resource "azurerm_kubernetes_cluster_node_pool" "win101" {
   mode                  = "User"
   name                  = "win101"
   orchestrator_version  = data.azurerm_kubernetes_service_versions.current.latest_version
-  os_disk_size_gb       = 30
+  os_disk_size_gb       = 35
   os_type               = "Windows" # Default is Linux, we can change to Windows
   vm_size               = "Standard_DS2_v2"
   priority              = "Regular"  # Default is Regular, we can change to Spot with additional settings like eviction_policy, spot_max_price, node_labels and node_taints
