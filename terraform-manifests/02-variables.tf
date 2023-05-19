a8b0271f1aa547b073b23c2c335b81dbc44685b4
LuchoN83 <39603304+LuchoN83@users.noreply.github.com>
2023-05-19 15:44:55 -0500

    Update 02-variables.tf

--- terraform-manifests/02-variables.tf
+++ terraform-manifests/02-variables.tf
@@ -46,4 +46,17 @@ variable "windows_admin_password" {
   default = "P@ssw0rd123456"
   description = "This variable defines the Windows admin password k8s Worker nodes"  
 }
+variable "cosmosdbaccountname" {
+  type = string
+  default = "retocosmosdb-beta"
+}
 
+variable "cosmosdbname" {
+  type = string
+  default = "DemoDB-beta"
+}
+
+variable "cosmosdbcollection" {
+  type = string
+  default = "DemoCollection-beta"
+}
