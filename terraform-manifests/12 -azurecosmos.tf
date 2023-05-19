57238055fab9892fc48b9c82ccf5535ad07c628b
Luis Navarrete <luis.navarrete.aranda@hotmail.com>
2023-05-19 00:14:42 -0500

    Cambio de Parametros para CosmoDB kind MONGODB 01

--- terraform-manifests/12 -azurecosmos.tf
+++ terraform-manifests/12 -azurecosmos.tf
@@ -15,17 +15,23 @@ resource "azurerm_cosmosdb_account" "db" {
   }
 }
 
-resource "azurerm_cosmosdb_sql_database" "db" {
+resource "azurerm_cosmosdb_mongo_database" "db" {
   name = var.cosmosdbname
   resource_group_name = azurerm_resource_group.aks_rg.name
   account_name = azurerm_cosmosdb_account.db.name
+  throughput = 400
 }
 
-resource "azurerm_cosmosdb_sql_container" "container" {
-  name = var.cosmosdbcontainer
+resource "azurerm_cosmosdb_mongo_collection" "collection" {
+  name = var.cosmosdbcollection
   resource_group_name = azurerm_resource_group.aks_rg.name
   account_name = azurerm_cosmosdb_account.db.name
-  database_name = azurerm_cosmosdb_sql_database.db.name
-  partition_key_path = "/id"
-  throughput = 400
+  database_name = azurerm_cosmosdb_mongo_database.db.name
+  default_ttl_seconds = "777"
+  shard_key           = "uniqueKey"
+  throughput          = 400
+  index {
+    keys   = ["_id"]
+    unique = true
+  }
 }
